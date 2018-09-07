07f78a631a7fb6d2769a13fddc04e68755d6922a
rossi56 <rossi56@hotmail.fr>
2018-09-07 10:13:30 +0200

    Résolution de bugs, navigation et map + lightbox

--- js/map.js
+++ js/map.js
@@ -1,18 +1,17 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: 47.675855,
-            lng: -2.765641
+            lat: 47.654955,
+            lng: -2.706508
           },
         zoom: 15
     });
-    // marker = new google.maps.Marker({
-    //     position: map.center,
-    //     map: map,
-    //     icon: "../images/logos/icon.png"
-    // });
+    marker = new google.maps.Marker({
+        position: map.center,
+        map: map,
+    });
 
-    // marker.setAnimation(google.maps.Animation.BOUNCE); //Animation des marqueurs
+    marker.setAnimation(google.maps.Animation.BOUNCE); //Animation des marqueurs
 
 
 }
--- js/script.js
+++ js/script.js
@@ -17,36 +17,10 @@ $(document).ready(function () {
       $('.burger').removeClass('open');
 
     });
+     /*==================================FAncy box================*/
+  $('#lightgallery').lightGallery({
+    pager: true
+  });
   });
 
-  /*==================================FAncy box================*/
-
-  $(document).ready(function() {
-
-    /* This is basic - uses default settings */
-    
-    $("a#single_image").fancybox({
-      'transitionIn'	:	'elastic',
-      'transitionOut'	:	'elastic',
-      'speedIn'		:	600, 
-      'speedOut'		:	200, 
-      'overlayShow'	:	false
-    });
-    
-    /* Using custom settings */
-    
-    $("a#inline").fancybox({
-      'hideOnContentClick': true
-    });
-  
-    /* Apply fancybox to multiple items */
-    
-    $("a.group").fancybox({
-      'transitionIn'	:	'elastic',
-      'transitionOut'	:	'elastic',
-      'speedIn'		:	600, 
-      'speedOut'		:	200, 
-      'overlayShow'	:	false
-    });
-    
-  });
+ 
